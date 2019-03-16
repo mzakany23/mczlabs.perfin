8255054fbbeef1cc771fa6e1ba01d1e412d5bc39
Mike Zakany <mzakany@gmail.com>
2019-03-16 11:36:02 -0400

    new test class ideas

--- tests/test_files.py
+++ tests/test_files.py
@@ -44,7 +44,7 @@ def setupdjango():
 
 
 @pytest.fixture
-def base_tests():
+def policy():
     '''
         TITLE
             Test learning about the file
@@ -54,120 +54,187 @@ def base_tests():
     '''
 
     return {
-        'policy' : {
-            # chase
-            "['Type', 'Trans Date', 'Post Date', 'Description', 'Amount']" : {
-                "name" : "CHASE",
-                "trim" : {
-                    "field" : "description",
-                    "value" : 10
-                }, 
-                "fields" : {
-                    "date" : 2,
-                    "description" : 3,
-                    "amount" : 4
-                }
-            },
-            # Fifth third
-            "['Date', 'Description', 'Check Number', 'Amount']" : {
-                "name" : "FIFTH_THIRD",
-                "trim" : {
-                    "field" : "description",
-                    "value" : 10
-                }, 
-                "fields" : {
-                    "date" : 0,
-                    "description" : 1,
-                    "check_num" : 2,
-                    "amount" : 3
-                }   
-            },
-            # Capital one
-            "[' Transaction Date', ' Posted Date', ' Card No.', ' Description', ' Category', ' Debit', ' Credit']" : {
-                "name" : "CAPITAL_ONE",
-                "trim" : {
-                    "field" : "description",
-                    "value" : 10
-                }, 
-                "fields" : {
-                    "date" : 1,
-                    "card" : 2,
-                    "description" : 3,
-                    "category" : 4,
-                    "amount" : 5,
-                    "credit" : 6,
-                }     
+        
+        # chase
+        "CHASE" : {
+            "header" : ['Type', 'Trans Date', 'Post Date', 'Description', 'Amount'],
+            "trim" : {
+                "field" : "description",
+                "value" : 10
+            }, 
+            "fields" : {
+                "date" : 2,
+                "description" : 3,
+                "amount" : 4
             }
         },
-        'data' : [
-            {
-                'should_be' : 'CHASE',
-                # header from file
-                'header' : ['Type','TransDate','PostDate','Description','Amount'],
-                # this is also given in file
-                'file_name' : 'mzakany-perfin/Chase3507_Activity20190314.CSV'
-            }
-        ]
+        # Fifth third
+        "FIFTH_THIRD" : {
+            "header" : ['Date', 'Description', 'Check Number', 'Amount'],
+            "trim" : {
+                "field" : "description",
+                "value" : 10
+            }, 
+            "fields" : {
+                "date" : 0,
+                "description" : 1,
+                "check_num" : 2,
+                "amount" : 3
+            }   
+        },
+        # Capital one
+        "CAPITAL_ONE" : {
+            "header" : [' Transaction Date', ' Posted Date', ' Card No.', ' Description', ' Category', ' Debit', ' Credit'],
+            "trim" : {
+                "field" : "description",
+                "value" : 10
+            }, 
+            "fields" : {
+                "date" : 1,
+                "card" : 2,
+                "description" : 3,
+                "category" : 4,
+                "amount" : 5,
+                "credit" : 6,
+            }     
+        }
     }
+        
 
-
+@pytest.fixture
+def scenarios():
+    return [
+        {
+            'should_be' : 'CHASE',
+            # header from file
+            'header' : ['Type','TransDate','PostDate','Description','Amount'],
+            # this is also given in file
+            'filename' : 'mzakany-perfin/Chase3507_Activity20190314.CSV'
+        }
+    ]
   
 # -------------------------------------------------------------------
 # 2. unit tests (es access stubbed)
 # -------------------------------------------------------------------
 
-
-class FileScore(object):
-    matches = []
-    
-    def update(self, domain, policy_header, header, file_name):
-        try:
-            header = '%s' % header
-            self.matches.append({
-                'domain' : domain,
-                'header_score' : fuzz.ratio(policy_header, header),
-                'filename_score' : fuzz.ratio(domain, file_name),
-            })
-        except:
-            self.not_in_index.append(1)
+class MalformedParams(Exception):
+    pass
 
 
-class FileType(object):
-
+class Base(object):
     def __init__(self, *args, **kwargs):
-        self.policy = kwargs.get('policy', [])
-        self.header = kwargs.get('header', [])
-        self.file_name = kwargs.get('file_name', [])
+        self.init_kwargs = kwargs 
+
+    def validate(self, keys):
+        kwargs = self.init_kwargs
+        if isinstance(kwargs, dict):
+            kwarg_keys = list(kwargs.keys())
+            listed = [key for key in keys if kwargs.get(key)]
+            if len(kwarg_keys) != len(listed):
+                raise MalformedParams('{} {} {}'.format(self.__class__.__name__, kwarg_keys, listed))
+            
+
+class FilePolicyManager(Base):
+    def __init__(self, *args, **kwargs):
+        super(FilePolicyManager, self).__init__(*args, **kwargs)
+        self.validate(['policy'])
+        self.policy = kwargs.get('policy')
+        self._policies = [FilePolicy(domain=k, policy_body=v) for k, v in self.policy.items()]
+
+    @property
+    def policies(self):
+        return self._policies
+        
+
+class FilePolicy(Base):
+    def __init__(self, *args, **kwargs):
+        super(FilePolicy, self).__init__(*args, **kwargs)
+        self.validate(['domain', 'policy_body'])
+        self.domain = kwargs.get('domain')
+        policy_body = kwargs.get('policy_body')
+        self.policy_header = policy_body['header']
+
+    @property
+    def serialized_header(self):
+        return '{}'.format(self.policy_header)
+    
+
+class FileMatchManager(Base):
+    _matches = {}
+    
+    def add(self, file):
+        total_score = sum([file.header_score, file.filename_score])
+        self._matches[total_score] = file 
+
+    @property
+    def matches(self):
+        return sorted(self._matches.items(), key=operator.itemgetter(0), reverse=True)
     
     @property
-    def score(self):
-        return self.calculate_file_score()
-    
+    def descending(self):
+        return [item[1] for item in self.matches]
+
+
+class FileMatch(Base):
+    def __init__(self, *args, **kwargs):
+        super(FileMatch, self).__init__(*args, **kwargs)
+        self.validate(['domain', 'filename', 'headers'])
+        
+        self.domain = kwargs.get('domain')
+        self.filename = kwargs.get('filename')
+        self.headers = kwargs.get('headers')
+        
+        if self.headers and self.filename and self.domain:
+            self.header_score = self.get_score(self.headers[0], self.headers[1])
+            self.filename_score = self.get_score(self.domain, self.filename)
+        
+    def get_score(self, one, two):
+        return fuzz.ratio(one, two)
+        
+
+class FileAnalyzer(Base):
+    def __init__(self, *args, **kwargs):
+        super(FileAnalyzer, self).__init__(*args, **kwargs)
+        self.validate(['header', 'filename', 'policy'])
+
+        policy = kwargs.get('policy')
+        self.header = kwargs.get('header', [])
+        self.filename = kwargs.get('filename', [])
+        self.policy_manager = FilePolicyManager(policy=policy)
+
     @property
     def top_match(self):
-        return self.score[0]
+        score = self.calculate_score() 
+        return score[0] if len(score) > 0 else None
 
-    def calculate_file_score(self):
-        score = FileScore()
-        for policy_header, policy_key in self.policy.items():
-            score.update(policy_key['name'], policy_header, self.header, self.file_name)
-        
-        first_sort = sorted(score.matches, key=operator.itemgetter('header_score'), reverse=True)
-        return sorted(first_sort, key=operator.itemgetter('filename_score'), reverse=True)
-        
-
-def test_unit(base_tests):
-    '''run tests'''
-    policy = base_tests['policy']
-    data_item = base_tests['data'][0]
-
-    file_type = FileType(
-        policy=policy,
-        file_name=data_item['file_name'],
-        header=data_item['header'],
-    )
+    @property
+    def serialized_header(self):
+        return '{}'.format(self.header)
     
-    assert file_type.top_match['domain'] == data_item['should_be']
+    def calculate_score(self):
+        matches = FileMatchManager()
+        
+        for policy in self.policy_manager.policies:
+            file_match = FileMatch(
+                domain=policy.domain,
+                filename=self.filename,
+                headers=[policy.serialized_header, self.serialized_header],
+            )
+            matches.add(file_match)
+        return matches.descending
+
+    
+def test_unit(policy, scenarios):
+    '''run tests'''
+    
+    for scenario in scenarios:
+        file_type = FileAnalyzer(
+            policy=policy,
+            filename=scenario['filename'],
+            header=scenario['header'],
+        )
+
+        assert file_type.top_match.domain == scenario['should_be']
     
 
 # -------------------------------------------------------------------
