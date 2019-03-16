2902da1d31d55a36aa5f6ab55be31427a09acb6b
Mike Zakany <mzakany@gmail.com>
2019-03-16 13:12:00 -0400

    one function

--- util/support.py
+++ util/support.py
@@ -1,8 +1,16 @@
 import hashlib
 import re
 
+
+def shorten_filename(string, word):
+    regex = r'{}'.format(word.lower())
+    found = re.search(regex, string.lower())
+    return found.group() if found else string
+    
+
 def strip_white(string):
     return re.sub(r'\s+', '', string)
+
 
 def generate_specific_key(key_string):
     """support function"""
