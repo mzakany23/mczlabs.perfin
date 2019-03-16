ab1add116a44b7c427f0611abf28a71b1ec12c1f
Mike Zakany <mzakany@gmail.com>
2019-03-16 13:11:36 -0400

    move to classes

--- util/classes.py
+++ util/classes.py
@@ -1,5 +1,133 @@
-from .support import strip_white
+from .support import strip_white, shorten_filename
+from fuzzywuzzy import fuzz
+import operator
 from dateutil.parser import parse
+
+
+class MalformedParams(Exception):
+    pass
+
+
+class Base(object):
+    def __init__(self, *args, **kwargs):
+        self.init_kwargs = kwargs 
+
+    def validate(self, keys):
+        kwargs = self.init_kwargs
+        if isinstance(kwargs, dict):
+            kwarg_keys = list(kwargs.keys())
+            listed = [key for key in keys if kwargs.get(key)]
+            if len(kwarg_keys) != len(listed):
+                raise MalformedParams('{} {} {}'.format(self.__class__.__name__, kwarg_keys, listed))
+            
+
+class FilePolicyManager(Base):
+    def __init__(self, *args, **kwargs):
+        super(FilePolicyManager, self).__init__(*args, **kwargs)
+        self.validate(['policy'])
+        self.policy = kwargs.get('policy')
+        self._policies = [FilePolicy(domain=k, policy_body=v) for k, v in self.policy.items()]
+
+    @property
+    def policies(self):
+        return self._policies
+        
+
+class FilePolicy(Base):
+    def __init__(self, *args, **kwargs):
+        super(FilePolicy, self).__init__(*args, **kwargs)
+        self.validate(['domain', 'policy_body'])
+        self.domain = kwargs.get('domain')
+        policy_body = kwargs.get('policy_body')
+        self.policy_header = policy_body['header']
+
+    @property
+    def serialized_header(self):
+        return '{}'.format(self.policy_header)
+    
+
+class FileMatchManager(Base):
+    _matches = {}
+    
+    def add(self, file):
+        total_score = file.total_score
+        self._matches[total_score] = file 
+
+    @property
+    def matches(self):
+        return sorted(self._matches.items(), key=operator.itemgetter(0), reverse=True)
+    
+    @property
+    def descending(self):
+        return [item[1] for item in self.matches]
+
+
+class FileMatch(Base):
+    base_stat_fields = [
+        'header_score',
+        'filename_score',
+    ]
+
+    def __init__(self, *args, **kwargs):
+        super(FileMatch, self).__init__(*args, **kwargs)
+        self.validate(['domain', 'filename', 'headers'])
+        
+        self.domain = kwargs.get('domain')
+        self.filename = shorten_filename(kwargs.get('filename'), self.domain)
+        self.headers = kwargs.get('headers')
+        
+        self.header_score = self.get_score(self.headers[0], self.headers[1])
+        self.filename_score = self.get_score(self.domain.lower(), self.filename)
+
+        if self.header_score == 100:
+            self.header_score += 50
+
+        if self.filename_score == 100:
+            self.filename_score += 50
+        if self.filename_score > 80:
+            self.filename_score += 10
+        else:
+            self.header_score -= 30
+            self.filename_score -= 50
+    
+    @property
+    def total_score(self):
+        return sum([getattr(self, key) for key in self.base_stat_fields])
+    
+    def get_score(self, one, two):
+        return fuzz.ratio(one, two)
+        
+
+class FileAnalyzer(Base):
+    def __init__(self, *args, **kwargs):
+        super(FileAnalyzer, self).__init__(*args, **kwargs)
+        self.validate(['header', 'filename', 'policy'])
+
+        policy = kwargs.get('policy')
+        self.header = kwargs.get('header', [])
+        self.filename = kwargs.get('filename', [])
+        self.policy_manager = FilePolicyManager(policy=policy)
+
+    @property
+    def top_match(self):
+        score = self.calculate_score() 
+        return score[0] if len(score) > 0 else None
+
+    @property
+    def serialized_header(self):
+        return '{}'.format(self.header)
+    
+    def calculate_score(self):
+        matches = FileMatchManager()
+        
+        for policy in self.policy_manager.policies:
+            file_match = FileMatch(
+                domain=policy.domain,
+                filename=self.filename,
+                headers=[policy.serialized_header, self.serialized_header],
+            )
+            matches.add(file_match)
+        return matches.descending
 
 
 class RowFactory(object):
