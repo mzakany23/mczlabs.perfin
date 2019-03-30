138b6efc13bed30c919a31c73f8f4a5dc59ac0c6
Mike Zakany <mzakany@gmail.com>
2019-03-29 23:13:42 -0400

    files

--- handler.py
+++ handler.py
@@ -1,8 +1,7 @@
 import os
 import sentry_sdk
-from s3fs.core import S3FileSystem
-from util.csv_functions import read_classified_file, open_and_yield_csv_row
-from util.es import get_es_connection, insert_document, create_index, perfin_schema, delete_index
+from util.csv_functions import read_classified_file
+from util.es import get_es_connection, insert_document
 
 
 ES_CONN = get_es_connection()
@@ -15,43 +14,25 @@ if sentry_key:
 S3 = os.environ.get("s3", True)
 
 
-
-def insert_files(file_paths, index):
+def insert_files(file_paths, index, **kwargs):
+    insert_fn = kwargs.get('insert_fn', insert_document)
+    read_csv_fn = kwargs.get('read_csv_fn', read_classified_file)
     for file_path in file_paths:
-        for row in read_classified_file(file_path, s3=S3):
+        for row in read_csv_fn(file_path, s3=S3):
             document = row["document"]
             document["group"] = row["_group"]
             print(document)
-            insert_document(ES_CONN, index, row["_id"], document)
+            insert_fn(ES_CONN, index, row["_id"], document)
 
 
-def insert_file(event, context):
+def process_files(event, context, **kwargs):
+    insert_fn = kwargs.get('insert_fn', insert_document)
+    read_csv_fn = kwargs.get('read_csv_fn', read_classified_file)
     records = event["Records"]
+    file_paths = []
     for record in records:
         file_name = record["s3"]["object"]["key"]
         bucket_name = record["s3"]["bucket"]["name"]
         file_path = "%s/%s" % (bucket_name, file_name)
-        index = "transactions_write"
-        for row in read_classified_file(file_path, s3=S3):
-            document = row["document"]
-            document["group"] = row["_group"]
-            insert_document(ES_CONN, index, row["_id"], document)
-
-
-def flush():
-    delete_index(ES_CONN, 'transactions')
-    create_index(ES_CONN, 'transactions', perfin_schema)
-
-
-def init():
-    s3 = S3FileSystem(anon=False)
-    files = s3.ls(path="mzakany-perfin")
-    insert_files(files, "transactions_write")
-
-
-if __name__ == "__main__":
-    # flush()
-    # init()
-    pass
-    
-    
+        file_paths.append(file_path)
+    insert_files(file_paths, 'transactions_write', **kwargs)
