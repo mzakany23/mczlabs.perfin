f0c371a402330eed2b91ca0ad3ec526d4ea6c1fa
mzakany23 <mzakany@gmail.com>
2019-12-07 13:55:04 -0500

    files

--- handler.py
+++ handler.py
@@ -16,19 +16,18 @@ if sentry_key:
 S3 = os.environ.get("s3", True)
 
 
-def insert_files(file_paths, index, **kwargs):
+def insert_files(es, file_paths, index, **kwargs):
     insert_fn = kwargs.get('insert_fn', insert_document)
     read_csv_fn = kwargs.get('read_csv_fn', read_classified_file)
+    read_from_s3 = kwargs.get('read_from_s3', True)
     for file_path in file_paths:
-        for row in read_csv_fn(file_path, s3=S3):
+        for row in read_csv_fn(file_path, s3=read_from_s3):
             document = row["document"]
             document["group"] = row["_group"]
-            insert_fn(ES_CONN, index, row["_id"], document)
+            insert_fn(es, index, row["_id"], document)
 
 
 def process_files(event, context, **kwargs):
-    insert_fn = kwargs.get('insert_fn', insert_document)
-    read_csv_fn = kwargs.get('read_csv_fn', read_classified_file)
     records = event["Records"]
     file_paths = []
     for record in records:
@@ -36,4 +35,4 @@ def process_files(event, context, **kwargs):
         bucket_name = record["s3"]["bucket"]["name"]
         file_path = "%s/%s" % (bucket_name, file_name)
         file_paths.append(file_path)
-    insert_files(file_paths, 'transactions_write', **kwargs)
+    insert_files(ES_CONN, file_paths, 'transactions_write', **kwargs)
